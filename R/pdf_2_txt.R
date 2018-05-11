d78c1c3c0a2f046db5225faa10adbbe127252f82
dong87 <dongleiming2008@gmail.com>
2018-05-11 12:00:51 +0800

    mod pdf2txt

--- R/pdf_2_txt.R
+++ R/pdf_2_txt.R
@@ -2,16 +2,18 @@
 #'
 #' Give the pdf file path and name to this function and wait a while (depends on the file numbers), the plain txts will be yielded.
 #' @param myfiles pdf file path and name
+#' @param pdf2text_path path of pdftotext.exe
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #' filenames <- "D:/R/Mendeley_PDF/Nature Genetics/filename.pdf"
-#' pdf_2_txt(filenames)
+#' pdf2text_path <- "C:/PATH/pdftotext.exe"
+#' pdf_2_txt(filenames, pdf2text_path)
 
-pdf_2_txt <- function(myfiles) {
-  txt <- lapply(myfiles, function(i) system(paste("./exe/pdftotext.exe", paste0("\"",
+pdf_2_txt <- function(myfiles, pdf2text_path) {
+  txt <- lapply(myfiles, function(i) system(paste(pdf2text_path, paste0("\"",
                                                                                 i, "\"")), wait = FALSE))
   msg(cli::rule(center = crayon::bold("Plz waiting for all PDFs transfered to TXTs!")),
       startup = TRUE)
